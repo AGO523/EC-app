521df010ebaf3b627dcb9d0b66742a7d5c97827f
AGO523 <unpouletfrais0523@gmail.com>
2022-02-19 21:19:52 +0900

    add: useEffect_to_Restaurants

--- frontend/src/apis/restaurants.js
+++ frontend/src/apis/restaurants.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { restaurantsIndex } from '../urls/index'
 
 export const fetchRestaurants = () => {
+  // axios.getの引数には文字列が必要で、ここではHTTPリクエストをなげる先のURL文字列が必要
   return axios.get(restaurantsIndex)
     .then(res => {
       return res.data
