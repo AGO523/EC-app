1c6d770a81fecb199453a15d848a1a7cee437e7b
AGO523 <unpouletfrais0523@gmail.com>
2022-02-22 06:15:29 +0900

    fix: restaurants.jsにコメントを追加

--- frontend/src/reducers/restaurants.js
+++ frontend/src/reducers/restaurants.js
@@ -1,7 +1,12 @@
 import { REQUEST_STATE } from '../constants';
 
+// initialStateは初期state
+// これをuseReducderに渡す
+// initialStateには２つの値が入る
 export const initialState = {
+  // 1つ目がGET APIの状態を表すfetchState
   fetchState: REQUEST_STATE.INITIAL,
+  // APIから取得したレストラン一覧が入ってきます。初期値は空配列として、[]を入れておきます。
   restaurantsList: [],
 };
 
@@ -10,6 +15,8 @@ export const restaurantsActionTyps = {
   FETCH_SUCCESS: 'FETCH_SUCCESS'
 }
 
+// API取得中 => fetchStateはLOADINGにスイッチする
+// API取得完了 => fetchStateをOKにスイッチし、restaurantsListにデータを入れる
 export const restaurantsReducer = (state, action) => {
   switch (action.type) {
     case restaurantsActionTyps.FETCHING:
@@ -26,3 +33,6 @@ export const restaurantsReducer = (state, action) => {
       throw new Error();
   }
 }
+// reducer関数はstateとactionを引数にとります。これはuseReducerで使われるreducer関数としては固定、と考えていいでしょう。
+// stateとは初期状態であれば先ほど定義したinitialStateが、あるいは加工後のstateが入ります。またactionにはreducerを使う側が指定したrestaurantsActionTypsのいずれかが入ります。
+// そして、restaurantsReducerは指定されたaction.typeに沿って、加工されたstateを返します。
