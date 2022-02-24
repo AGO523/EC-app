27245daef7696fb845133da9d7211662f635de37
AGO523 <unpouletfrais0523@gmail.com>
2022-02-24 21:31:12 +0900

    add: line_foods api

---
+++ frontend/src/apis/line_foods.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { lineFoods } from '../urls/index'
+
+// URLに対してPOSTリクエストを送りたいので、axios.post()を使います。引数にリクエスト先のURL文字列、そして第二引数にパラメーターを渡します。今回はfood_idとcountの２つをオブジェクト形式で渡します。
+// こうすることでサーバーサイドではparams[:food_id], params[:count]のかたちでそれぞれの値を読み取ることができます。
+export const postLineFoods = (params) => {
+  return axios.post(lineFoods,
+    {
+      food_id: params.foodId,
+      count: params.count,
+    }
+  )
+    .then(res => {
+      return res.data
+    })
+    .catch((e) => { throw e; })
+};
